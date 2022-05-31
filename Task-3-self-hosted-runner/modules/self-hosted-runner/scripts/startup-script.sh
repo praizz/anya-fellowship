9b7904050181d1799ce7a9173c4bfe374a7d4a10
praizz <praiseadanlawo@gmail.com>
2022-05-31 22:26:18 +0100

    upgrade runner version

--- Task-3-self-hosted-runner/modules/self-hosted-runner/scripts/startup-script.sh
+++ Task-3-self-hosted-runner/modules/self-hosted-runner/scripts/startup-script.sh
@@ -3,9 +3,9 @@ cat <<EOF >/home/ubuntu/user-data.sh
 #!/bin/bash
 mkdir ~/actions-runner
 cd ~/actions-runner
-curl -O -L https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz
-tar xzf ~/actions-runner/actions-runner-linux-x64-2.263.0.tar.gz
-rm ~/actions-runner/actions-runner-linux-x64-2.263.0.tar.gz
+curl -O -L https://github.com/actions/runner/releases/download/v2.291.1/actions-runner-linux-x64-2.291.1.tar.gz
+tar xzf ~/actions-runner/actions-runner-linux-x64-2.291.1.tar.gz
+rm ~/actions-runner/actions-runner-linux-x64-2.291.1.tar.gz
 ~/actions-runner/config.sh --url ${repo_url} --token ${github_access_token} --name "Github EC2 Runner" --unattended
 ~/actions-runner/run.sh
 EOF
