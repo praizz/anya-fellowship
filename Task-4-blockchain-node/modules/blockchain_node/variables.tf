2d79bf56dc27ba8dc2dba0cc1b06ab348342ba6c
Derek Crosson <derekcrosson18@gmail.com>
2022-06-04 12:13:17 +0200

    chore: remove ssh key

--- Task-4-blockchain-node/modules/blockchain_node/variables.tf
+++ Task-4-blockchain-node/modules/blockchain_node/variables.tf
@@ -15,7 +15,7 @@ variable "machine_type" {
 }
 
 variable "public_key" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzHVMV5vSGeoCZD9gzdPCpSkJcSH3b62mvZ0ZaQixkHu6SSCFdflA8CF4XKeqV/s4Jl+++8xeThNm8tMUT1nYNXlK0eCYmTZID/B75aI4aThXavzEJAxQTQ+1rl1Iiz8ydnFF/zexLOSonDNOZNbMGJylPPk3Wjgenpa1ezHmiZCNck+41/iNpTsrkel74CPmzuD6HWed4NCCZUnGAyHNAa3bXWBu0Uc6QsjCTFn5GXNg2SC1cgZTWY5Eli/HM1nQjt/xUHqqBgrotGRxoAwvWcUx4nlapCHmoftUw5akH20wnMx+AjovE27wM74fJNdAFtmw5HTRaS4RgVyFgy2v7 derek@Dereks-Air.Dlink"
+  default = "ssh-rsa PUT KEY HERE"
 }
 
 variable "ssh_user" {
