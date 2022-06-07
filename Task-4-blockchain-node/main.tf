b238cdf3b5e3cc26a4be3d5d6719edf8fa54f91e
Derek Crosson <derekcrosson18@gmail.com>
2022-06-07 10:50:17 +0200

    refactor: remove remote state

--- Task-4-blockchain-node/main.tf
+++ Task-4-blockchain-node/main.tf
@@ -1,10 +1,3 @@
 module "blockchain_node" {
   source    = "./modules/blockchain_node"
 }
-
-
-module "remote-state-locking" {
-  source              = "./modules/remote_state"
-  name_prefix          = "anya-fellowship-blockchain node"
-  backend_output_path = "./backend.tf"
-}
--- Task-4-blockchain-node/modules/remote_state/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "backend_output_path" {
-  default     = "./backend.tf"
-  description = "file path to populate the backend configuration"
-}
-
-variable "name_prefix" {
-  default     = ""
-  description = "s3 bucket name prefix"
-}
